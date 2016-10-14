54609dfbe5d5337a4a5d81b7217f177981ef72ab
lnara002 <lakshman_kumar.narayanan@nokia.com>
2016-10-14 16:55:10 -0700

    whatever i have

--- grep_in_c_code.sh
+++ grep_in_c_code.sh
@@ -5,15 +5,37 @@ usage()
   echo "$1 .. grep regex and also report the c-function its found in .."
   echo "      It prepares a grepOp file that is loaded at this pwd in vim's quickfix window"
   echo ""
-  echo "Usage: $1 <sub-dir> <regexp>"
+  echo "Usage: $1 -d <sub-dir> -f <files-list> -p <prepend> <regexp>"
   echo ""
-  echo " <sub-dir>      look only this sub-dir"
+  echo " <sub-dir>      look only this sub-dir. Default is to use ./"
+  echo " <files-list>   the list of files to grep as in this file"
+  echo " <prepend>      prepend this dir to all files in files-list"
   echo " <regexp>       the regexp"
   exit 1;
 }
 
-while getopts "x:p:h" opt; do
+files_list=""
+prepend=""
+
+while getopts "f:d:h" opt; do
   case $opt in
+    f)
+      files_list=$OPTARG
+      if [ ! -f $files_list ] ; then
+         echo "file $files_list doesn't seem to be a regular file"
+         exit 1
+      fi
+      ;;
+    p)
+      prepend=$(OPTARG)
+      ;;
+    d) 
+      dir=$OPTARG
+      if [ ! -d $dir ] ; then
+         echo "$dir doesn't seem to be a regular directory"
+         exit 1
+      fi
+      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       usage $0
@@ -28,14 +50,6 @@ done
 shift $((OPTIND-1))
 
 if [ -z "$1" ] ; then
-  echo "Supply a dir. Use ./ if u want entire pwd!"
-  usage $0
-fi
-
-SUBDIR=$1
-shift
-
-if [ -z "$1" ] ; then
   echo "Supply a Regexp!"
   usage $0
 fi
@@ -43,14 +57,22 @@ fi
 REGEX=$1
 shift
 
+if [ -n "$files_list" ] ; then
+  files=$(cat $files_list)
+else 
+  if [ -n "$dir" ] ; then 
+    cd $dir
+  fi
+  files=$(find . -name '*.[ch]')
+fi
+
 # assumptions (dumb, but works most of the time and is fairly efficient!):
 #   1. A function-start is simply a line with a { at column-1
 #   2. A function-name is simply that matches [[:alnum:]]\+[::space:]]*( in it.
 #   3. The current-function-name is the latest function-name preceding a function-start
 
-a=$(find . -name '*.[ch]')
 
-for i in $a ; do 
+for i in $files ; do 
 
   gawk -v re=$REGEX -v file=$i '
 
